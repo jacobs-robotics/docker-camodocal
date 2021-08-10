99c3bf6add8a92475abba12e3ecea946ceffa382
WasteAntAdmin@MKK <>
2021-08-10 14:35:53 +0200

    Add possibility to enter the container to access all funcionalities

--- README.md
+++ README.md
@@ -32,19 +32,34 @@ After cloning the repository, run the next command (UNIX systems):
 After the container is built; run the following command:
 
 ```
-./docker_helper.sh build run [CAMODOCAL_COMMAND]
+./docker_helper.sh --build [CAMODOCAL_COMMAND]
 ```
 
 where CAMODOCAL_COMMAND is a string with the instruction and parameters needed by camodocal as defined in the [CamOdoCal repository](https://github.com/hengli/camodocal). For example:
 
 ```
-./docker_helper.sh run "stereo_calib -w 8 -h 6 -s 0.06 --camera-model kannala-brandt -i /root/input_data -o /root/output_data"
+./docker_helper.sh --run-rm "stereo_calib -w 8 -h 6 -s 0.06 --camera-model kannala-brandt -i /root/input_data -o /root/output_data"
 ```
 
 The */input_data* and */output_data* directories are mounted to the docker container for the user to save the files needed to run these commands as well as their output. Please, be sure to add the */root* parent directory when writing the path to these directories. You can always run the *--help* command to see a list of available parameters:
 
 ```
-./docker_helper.sh run "intrinsic_calib --help"
+./docker_helper.sh --run-rm "intrinsic_calib --help"
+```
+
+#### ATTENTION/BUG
+
+When using the ```intrinsic_calib``` command it was pointed out that the output files cannot be access by the host. Unfortunately the original Camodocal software hardcodes the output directory and filenames for this use case. 
+
+To enable users of this docker version use the "intrinsic calibration", the next command allows the user to enter the created container in the directory of the Camodocal executables to run them as they would natively. The user will have to manually move the output files into the */root/output_data* directory in the container so that the host can access them. Example:
+
+```
+hostuser@hostmachine:~/docker-camodocal$ ./docker_helper.sh --run-keep
+root@hostcamodocal:~/camodocal/build/bin# ./intrinsic_calib -w 8 -h 6 -s 0.06 --opencv --camera-model pinhole -p img_ -e .png -i /root/input_data/my_images
+root@hostcamodocal:~/camodocal/build/bin# mv camera* ../../../output_data/.
+root@hostcamodocal:~/camodocal/build/bin# chmod -R a+rw ../../../output_data/*
+root@hostcamodocal:~/camodocal/build/bin# exit
+hostuser@hostmachine:~/docker-camodocal$
 ```
 
 ## Tips & Comments
--- docker_helper.sh
+++ docker_helper.sh
@@ -12,19 +12,23 @@ group=`id -g -n`
 groupid=`id -g`
 myhostname=hostcamodocal
 no_proc=`nproc`
+flag_issue_command=0
 
-if [ $1 = "help" ];then
+if [ $1 = "--help" ];then
 	echo -e "${GREEN}>>> Possible commands:\n ${NC}"
-	echo -e "${BLUE}build --- Build an image based on DockerFile in current dir\n"
-	echo -e "${BLUE}run --- Create and run container from image${NC}\n"
+	echo -e "${BLUE}--build		--- Build an image based on DockerFile in current dir\n"
+	echo -e "${BLUE}--run-rm	--- Create--> run --> execute command --> remove and exit container${NC}\n"
+	echo -e "${BLUE}--run-keep	--- Create--> run --> enter container and bash (container is removed when user exits the container)${NC}\n"
+	flag_issue_command=1
 fi
 
-if [ "$1" = "build" ]; then
+if [ "$1" = "--build" ]; then
 	echo -e "${GREEN}>>> Building camodocal image ...${NC}"
 	docker build -t ${user}/${containerName}:${containerTag} .
+	flag_issue_command=1
 fi
 
-if [ "$1" = "run" ]; then
+if [ "$1" = "--run-rm" ]; then
 
 	echo -e "${GREEN}>>> Initializing "${containerName}" container...${NC}"
 	
@@ -51,8 +55,52 @@ if [ "$1" = "run" ]; then
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--volume=`pwd`/input_data:/root/input_data:rw \
 	--volume=`pwd`/output_data:/root/output_data:rw \
-	${user}/${containerName}:${containerTag} /bin/bash -c "cd /root/camodocal/build/bin && ./$2 && chmod a+rw /root/output_data/*"
+	${user}/${containerName}:${containerTag} /bin/bash -c "cd /root/camodocal/build/bin && ./$2 && chmod -R a+rw /root/output_data/*"
 	rc=$?; if [[ $rc != 0 && $rc != 1 ]]; then exit $rc; fi
 
+	flag_issue_command=1
+fi
 
+if [ "$1" = "--run-keep" ]; then
+
+	echo -e "${GREEN}>>> Initializing "${containerName}" container...${NC}"
+	
+	#check for host devices
+	DRI_ARGS=""
+	if [ -d /dev/dri ]; then
+
+		for f in `find /dev/dri -type c`; do
+				DRI_ARGS="$DRI_ARGS --device=$f"
+		done
+
+		DRI_ARGS="$DRI_ARGS --privileged"
+	fi
+
+
+	docker run --rm -it --gpus all \
+	$DRI_ARGS \
+	--name="${containerName}" \
+	--hostname="${myhostname}" \
+	--net=default \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--workdir="/root" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume=`pwd`/input_data:/root/input_data:rw \
+	--volume=`pwd`/output_data:/root/output_data:rw \
+	${user}/${containerName}:${containerTag} /bin/bash -c "cd /root/camodocal/build/bin && /bin/bash"
+	rc=$?; if [[ $rc != 0 && $rc != 1 ]]; then exit $rc; fi
+
+	flag_issue_command=1
+fi
+
+if [ "$1" == "--terminal" ] || [ "$1" == "-t" ]; then
+    echo -e "${GREEN}>>> Entering console in container ${container_name} ...${NC}"
+    docker exec -ti ${container_name} /bin/bash -c "cd /root/camodocal/build/bin && /bin/bash"
+    flag_issue_command=1
+fi
+
+if [[ $flag_issue_command == 0 ]]; then
+    echo -e "${RED}>>>ERROR: Unknown or badly placed parameter ...$NC" >&2
+    exit 1
 fi
